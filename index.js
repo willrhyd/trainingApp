070ef504bb1879ae77ad438adc777f0d54f131be
willrhyd <34925929+willrhyd@users.noreply.github.com>
2021-10-01 11:57:46 +0100

    enable cookies

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.use(session({
     secure: true,
     maxAge: 100 * 60 * 60 * 24,
     sameSite: 'none',
-    domain: 'rides.trainingappserver.uk',
+    domain: 'trainingappserver.uk',
     httpOnly: false
   },
   unset: 'destroy',
